ec9342e4779def539520c00a74255032ed59fbaf
Aditya Hari <aditya.hari.ft21@mail.umy.ac.id>
2022-10-31 15:41:23 +0700

    Add project files.

---
+++ Exercise2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    internal class Program
+    {
+        int[] adit = new int[64];
+        static void Main(string[] args)
+        {
+        }
+    }
+}
